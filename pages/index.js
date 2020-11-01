86d15464be74007317f3de78c55afd52ee5dc5e1
softmarshmallow <woojoo@softmarshmallow.com>
2020-11-01 15:55:59 +0900

    analytics test

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import theme from '../common/theme';
 import firebase from "../utils/firebase"
 
 export default function Home() {
+  firebase.analytics().logEvent('test')
   return (
     <div className={styles.container}>
       <Head>
--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,21 +1,15 @@
-import firebase from "firebase"
+import firebase from 'firebase/app';
 import "firebase/analytics"
 
 const config = {
     apiKey: process.env.FIREBASE_API_KEY,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    appId: process.env.FIREBASE_APP_ID,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
     storageBuket: process.env.FIREBASE_STORAGE_BUKET,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    apId: process.env.FIREBASE_APP_ID,
     measurementId: process.env.FIREBASE_MEASYREMENT_ID
 }
 
-
-if (typeof window !== "undefined" && !firebase.app.length) {
-    firebase.initializeApp(config)
-    if ('measurementId' in config) firebase.analytics()
-}
-
-export default firebase
+export default typeof window !== 'undefined' ? !firebase.apps.length ? firebase.initializeApp(config) : firebase.app() : undefined;
