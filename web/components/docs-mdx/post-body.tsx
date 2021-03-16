50704694d8214e6d2c82309ebae993ae20b13404
chaewoong0407 <an0304073@naver.com>
2021-03-16 23:16:15 +0900

    modify : markdown styling

--- web/components/docs-mdx/post-body.tsx
+++ web/components/docs-mdx/post-body.tsx
@@ -1,10 +1,52 @@
+import styled from '@emotion/styled';
+
 export default function PostBody({ content }) {
   return (
     <div className="max-w-2xl mx-auto">
-      <div
+      <MarkdownView
         // className={markdownStyles["markdown"]}
         dangerouslySetInnerHTML={{ __html: content }}
       />
     </div>
   );
 }
+
+
+const MarkdownView = styled.div`
+  h1 {
+      font-size: 64px;
+      font-weight: bold;
+      color: #000000;
+  } 
+
+  h2 {
+    font-size: 48px;
+    font-weight: bold;
+      color: #000000;
+  }
+
+  h3 {
+    font-size: 36px;
+    font-weight: bold;
+      color: #000000;
+  }
+
+  p {
+    font-size: 16px;
+    color: #686868;
+  }
+
+  code {
+    background: #1D1F21;
+    color: white;
+    border-radius: 4px;
+;
+  }
+
+  li {
+    font-size: 16px;
+    color: #686868;
+    padding-bottom: 1.5rem;
+  }
+`;
+
