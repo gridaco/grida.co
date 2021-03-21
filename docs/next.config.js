5a2962826e4ed9add95a096f81196821af95c949
softmarshmallow <woojoo@softmarshmallow.com>
2021-03-21 16:26:29 +0900

    set docs as mtzone root

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,3 +1,8 @@
-module.exports = {
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+});
+
+module.exports = withMDX({
   basePath: "/docs",
-};
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+});
