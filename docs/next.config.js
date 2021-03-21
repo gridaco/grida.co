54103cd8bb0ed0c8d5fe43f98a4d8e763b343ad3
softmarshmallow <woojoo@softmarshmallow.com>
2021-03-21 20:22:06 +0900

    remove base path

--- docs/next.config.js
+++ docs/next.config.js
@@ -3,20 +3,15 @@ const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
 });
 
-module.exports = withMDX(
-  // withTM(
-  {
-    basePath: "/docs",
-    transpileModules: ["shared-layouts", "styles"],
-    pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
-    async rewrites() {
-      return [
-        {
-          source: "/:path*",
-          destination: "/docs/:path*",
-        },
-      ];
-    },
+module.exports = withMDX({
+  transpileModules: ["shared-layouts", "styles"],
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "/docs/:path*",
+      },
+    ];
   },
-  // ),
-);
+});
