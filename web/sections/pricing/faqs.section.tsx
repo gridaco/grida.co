22f6aaf697aa8f0abd1d60a3bbf5ad637f22db0f
softmarshmallow <woojoo@softmarshmallow.com>
2022-01-01 16:27:33 +0900

    wip - contents update

--- web/sections/pricing/faqs.section.tsx
+++ web/sections/pricing/faqs.section.tsx
@@ -1,28 +1,31 @@
 import React from "react";
-import { FaqQnaItem } from "components/faq/interface";
+
 import FAQs from "components/faq";
+import { FaqQnaItem } from "components/faq/interface";
 
 const PRICING_FAQ_LIST: FaqQnaItem[] = [
   {
     query: "How do Grida make money?",
-    answer: "TBD",
+    answer:
+      "Grida makes money from Enterprise B2B plans. It's free and opensource for individual and startups.",
   },
   {
     query: "What are the limitations with free plan?",
-    answer: "TBD",
+    answer: "There are no limitations with the free plan.",
   },
   {
     query: "I cannot see images anymore. What happened?",
     answer:
-      "Your image is uploaded and hosted on Grida cloud for 24 hours for development mode. If you enable publishing option for the screen / component you selected, all the resources will be long-lived. long-lived resource hosting is only available for paid plan. for free plan, we only support 24 hours temp hosting.",
+      "Your image is uploaded and hosted on Grida cloud for 24 hours for development mode. If you enable publishing option for the screen / component you selected, all the resources will be long-lived. long-lived resource hosting is only available for paid plan. for free plan, you have to download the asset, add manually to your repository.",
   },
+  // {
+  //   query: "How does the standard extra cloud usage fee calculated?",
+  //   answer: "TBD",
+  // },
   {
-    query: "How does the standard extra cloud usage fee calculated?",
-    answer: "TBD",
-  },
-  {
-    query: "Does Grida have explicit enterprise support plan?",
-    answer: "TBD",
+    query: "Do I need enterprise plan?",
+    answer:
+      "If you are a team of 5 or more, We highly recommand to contact us for enterprise plan. You can benefit the most from the enterprise plan, by customized build that will match your existing design, working process.",
   },
 ];
 
